babddf30f91bc0ee6b6bee58441025490b4ee3f9
Alfredo <alfredohzimmermann@gmail.com>
2022-04-27 22:25:10 -0300

    exerciciosfeito

---
+++ MeusExercicios/MeuPrimeiroCodigo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MeuPrimeiroCodigo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double nota1, nota2, nota3;
+            double media;
+
+            Console.WriteLine("Digite a nota 1:");
+            Double.TryParse(Console.ReadLine(), out nota1);
+
+            Console.WriteLine("Digite a nota 2:");
+            Double.TryParse(Console.ReadLine(), out nota2);
+
+            Console.WriteLine("Digite a nota 3:");
+            Double.TryParse(Console.ReadLine(), out nota3);
+
+            media = (nota1 + nota2 + nota3) / 3;
+            Console.WriteLine("Sua media :");
+            Console.WriteLine(media);
+
+            if(media >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+
+
+           
+        }
+    }
+}
---
+++ MeusExercicios/MinhasInformações/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MinhasInformações
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = ("Alfredo");
+            int idade = 36;
+            decimal peso = 70;
+
+            Console.WriteLine(nome);
+            Console.WriteLine(idade);
+            Console.WriteLine(peso);
+
+            Console.ReadLine();
+
+           
+
+
+            
+
+
+
+            
+        }
+    }
+}
