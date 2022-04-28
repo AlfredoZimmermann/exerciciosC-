f6ade6fa9903973e9612ffa26d8a6dda7e11c802
Alfredo <alfredohzimmermann@gmail.com>
2022-04-27 23:01:53 -0300

    MeuPrimeiroCodigo.txt

--- MeusExercicios/MinhasInformações/Program.cs
+++ MeusExercicios/MinhasInformações/Program.cs
@@ -11,20 +11,34 @@ namespace MinhasInformações
             double peso = 71.7;
 
 
+            Console.WriteLine("Digite seu nome:");
+             nome = Console.ReadLine();
+
+            Console.WriteLine("Qual é sua idade");
+            int.TryParse(Console.ReadLine(), out idade);
+           
+            Console.WriteLine("Qual é seu peso:");
+            Double.TryParse(Console.ReadLine(), out peso);
+
+
+
+
+            Console.WriteLine("Seu nome e ");
             Console.WriteLine(nome);
+
+            Console.WriteLine(" Sua idade e ");
             Console.WriteLine(idade);
+
+            Console.WriteLine("Seu peso e ");
             Console.WriteLine(peso);
 
-            Console.ReadLine();
-
-           
-
-
-            
 
 
 
-            
+
+
+
+
         }
     }
 }
